e053f9e695d28749369cbc0c6d8be6b3a39700b7
Cameron Durham <cdurham@usc.edu>
2021-05-28 00:29:27 -0700

    Force tag update

--- cmd/root.go
+++ cmd/root.go
@@ -20,23 +20,14 @@ const (
 	RepositoryUrl = "https://github.com/camerondurham/ch"
 )
 
-// TODO: cleanup skeleton code
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:     "ch",
 	Short:   "A simple container helper to create and manage Docker environments",
 	Version: version.PkgVersion,
-	// 	Long: `A longer description that spans multiple lines and likely contains
-	// examples and usage of using your application. For example:
-
-	// Cobra is a CLI library for Go that empowers applications.
-	// This application is a tool to generate the needed files
-	// to quickly create a Cobra application.`,
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
+
 	// TODO: only check if user didn't specify they don't want upgrade reminders
 	latestVersion, err := util.GetLatestVersion(util.GetRequest)
 	if err != nil {
